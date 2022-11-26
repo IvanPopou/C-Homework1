ed1e9a95da48b1af8c28adde3033f25c5eb3ff4e
Ivan <ivaspo@yandex.ru>
2022-11-26 18:42:19 +0300

    Решена задача 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите число a > ");
+string inputA = Console.ReadLine();
+int valueA = Convert.ToInt32(inputA);
+Console.Write("Введите число b > ");
+string inputB = Console.ReadLine();
+int valueB = Convert.ToInt32(inputB);
+Console.Write("Введите число с > ");
+string inputC = Console.ReadLine(); 
+int valueC = Convert.ToInt32(inputC);
+
+int max = valueA;
+
+if (valueB>max) max = valueB;
+if (valueC>max) max = valueC;
+
+System.Console.WriteLine($"Максимальное число {max}");
